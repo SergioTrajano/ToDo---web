2eeb661d578697319bbe5f881a9141786810c5ee
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-06-04 15:45:47 -0300

    feat: :sparkles: integrate NewTask to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import { Header } from "./components/Header";
+import { useState } from "react";
 
+import { Header } from "./components/Header";
+import { NewTask } from "./components/NewTask";
+
+import styles from "./App.module.css";
 import "./global.module.css";
 
+type TaskProps = {
+    content: string;
+    isDone: boolean;
+};
+
 export function App() {
+    const [tasks, setTasks] = useState<TaskProps[]>([]);
+
+    function addTask(content: string) {
+        const newTask: TaskProps = {
+            content,
+            isDone: false,
+        };
+
+        setTasks((prev) => [...prev, newTask]);
+    }
+
     return (
-        <>
+        <div className={styles.wrapper}>
             <Header />
-        </>
+
+            <NewTask onNewTask={addTask} />
+        </div>
     );
 }
